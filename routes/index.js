64b9df18740c18e3a4fe17ab63431a0470cd9831
Anthony-zh-Zhang <31829495+Anthony-zh-Zhang@users.noreply.github.com>
2017-09-22 10:00:52 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var exec = require('exec');
+var Analyser = require('audio-analyser');
+var Generator = require('audio-generator');
 var AudioContext = require('web-audio-api').AudioContext
 
 context = new AudioContext
@@ -17,9 +19,9 @@ var pcmdata = [] ;
 
 // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 /* GET home page. */
-router.get('/', function(req, res, next) {
- res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+// });
 
 router.use('/public', express.static(__dirname + '/public'));
 
@@ -28,18 +30,22 @@ router.use('/public', express.static(__dirname + '/public'));
 // line 22-45 works fine!!!
 /** Implementing Simple Music Server using Express JS **/
 
-// router.get('/', function(req,res, next){
-// 	// File to be served
-//
-// 	//var fileId = req.query.id;
-// 	//var file = __dirname + '/music/' + fileId;
-//   var file =  './public/Jiangnan.mp3';
-// 	fs.exists(file,function(exists){
-// 		if(exists)
-// 		{
-//
-// 			var rstream = fs.createReadStream(file);
-// 			rstream.pipe(res);
+router.get('/', function(req,res, next){
+	// File to be served
+
+	//var fileId = req.query.id;
+	//var file = __dirname + '/music/' + fileId;
+  var file =  './public/Jiangnan.mp3';
+	fs.exists(file,function(exists){
+		if(exists)
+		{
+
+			var rstream = fs.createReadStream(file);
+//			rstream.pipe(res);
+        rstream.pipe(analyser);
+
+
+
 //       var ffmpeg = require('ffmpeg');
 //
 //       new ffmpeg('./public/Jiangnan.mp3', function (err, video) {
@@ -49,18 +55,65 @@ router.use('/public', express.static(__dirname + '/public'));
 //       			console.log('Error: ' + err);
 //       		}
 //       	});
-//       }
-//
-// 		else
-// 		{
-// 			res.send("Its a 404");
-// 			res.end();
-// 		}
-//
-// 	});
-//
-// });
+       }
 
+		else
+		{
+			res.send("Its a 404");
+			res.end();
+		}
+
+	});
+
+});
+
+var Analyser = require('audio-analyser');
+var Generator = require('audio-generator');
+var assert = require('assert');
+var almost = require('almost-equal');
+var err = 0.1;
+var pcm = require('pcm-util');
+
+var analyser = Analyser({
+	fftSize: 4
+});
+
+analyser.on('data', function (chunk) {
+	var floatFreq = this.getFloatFrequencyData(new Float32Array(this.fftSize));
+	var floatTime = this.getFloatTimeDomainData(new Float32Array(this.fftSize));
+	var byteFreq = this.getByteFrequencyData(new Uint8Array(this.fftSize));
+	var byteTime = this.getByteTimeDomainData(new Uint8Array(this.fftSize));
+	var freq = this.getFrequencyData();
+	var time = this.getTimeData();
+  console.log(byteTime);
+	// assert(almost(floatFreq[0], freq[0], err, err));
+	// // assert(almost(byteFreq[0], freq[0], err, err));
+	// assert.equal(floatFreq.length, freq.length);
+	// assert.equal(byteFreq.length, freq.length);
+  //
+	// assert(almost(floatTime[0], time[0], err, err));
+  //
+	// assert(almost(pcm.convertSample(byteTime[0], {signed: false, bitDepth: 8}, {float: true}), time[0], err, err));
+	// assert.equal(floatTime.length, time.length);
+	// assert.equal(byteTime.length, time.length);
+});
+
+// Generator Example
+
+
+// Generator({
+// 	generate: function () {
+// 		return Math.random();
+// 	},
+// 	samplesPerFrame: 32,
+// 	duration: 0.1
+// })
+//
+// .pipe(analyser);
+
+
+
+// shield for good !
 
 // var soundfile = "public/Jiangnan.mp3"
 // decodeSoundFile(soundfile);
@@ -70,10 +123,12 @@ router.use('/public', express.static(__dirname + '/public'));
 //     if (err) throw err
 //     context.decodeAudioData(buf, function(audioBuffer) {
 //       console.log(audioBuffer.numberOfChannels, audioBuffer.length, audioBuffer.sampleRate, audioBuffer.duration);
-//       pcmdata = (audioBuffer.getChannelData(0)) ;
+//       pcmdata = (audioBuffer.getChannelData(1)) ;
 //       samplerate = audioBuffer.sampleRate; // store sample rate
 //       maxvals = [] ; max = 0 ;
-//       playsound(soundfile)
+//
+//       analyser.getByteTimeDomainData(arr);
+//       //playsound(soundfile)
 //       findPeaks(pcmdata, samplerate)
 //     }, function(err) { throw err })
 //
@@ -119,9 +174,10 @@ router.use('/public', express.static(__dirname + '/public'));
 //   }
 //   return bars ;
 // }
-//
-//
-//
+
+
+// We do not play songs currenlty
+
 // function playsound(soundfile){
 // // linux or raspi
 // // var create_audio = exec('aplay'+soundfile, {maxBuffer: 1024 * 500}, function (error, stdout, stderr) {
